d8a92b644f9557a398a6ee56e4056df710d759a9
André Karge <andre.karge@uni-weimar.de>
2015-06-15 18:34:52 +0200

    added the support for all views

--- BauhausBoards.js
+++ BauhausBoards.js
@@ -2,6 +2,8 @@ var express = require('express');
 var stylus  = require('stylus');
 var nib     = require('nib');
 
+//Liberation Mono 12 :)
+
 //define to use the express framework
 var app = express();
 //use nib middleware for stylus
@@ -35,7 +37,85 @@ app.get('/', function (req, res) {
     condition : true }
   );
 });
-app.get('/test', function (req, res) {
+app.get('/message', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/message/editor', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/message/qr', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/status', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/content', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/messages', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/users', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/users/add', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/users/change', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/users/change/pass', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/room', function (req, res) {
+  res.render('test',
+  {
+
+  });
+});
+app.get('/backend/settings/logs', function (req, res) {
   res.render('test',
   {
 
