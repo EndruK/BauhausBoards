2a90ef596a17e38b07b837086bafe17a36aac263
André Karge <andre.karge@uni-weimar.de>
2015-06-23 18:39:14 +0200

    gewitter

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var stylus  = require('stylus');
+var nib     = require('nib');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/bauhausboards');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var functions = require('./routes/functions');
 
 var app = express();
+//use nib middleware for stylus
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,13 +28,19 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+//define stylus to compile styl to css
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/functions', functions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
