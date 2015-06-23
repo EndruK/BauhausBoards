dead9778ed392712f955e2de861cc333f866e993
André Karge <andre.karge@uni-weimar.de>
2015-06-23 16:55:29 +0200

    started a new express project

--- app.js
+++ app.js
@@ -4,31 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var stylus = require('stylus');
-var nib = require('nib');
-
-// New Code
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-
-//use nib middleware for stylus
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib());
-};
-//define stylus to compile styl to css
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,12 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
 
 app.use('/', routes);
 app.use('/users', users);
