b9379c2910dc14d9b8fc51d596186cd645e67204
André Karge <andre.karge@gmail.com>
2015-07-02 20:05:26 +0200

    added first button interaction and re-ordered the css class/id structure

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,28 +1,73 @@
 var pos = 'index';
-var sideBarTimeout = 20000; //sidebar timeout in ms
-
+var sideBarTimeout = 5000; //sidebar timeout in ms
+var sidebarTimeoutHandler;
 //###DOM#######################################
+$('.sidebar').on('click', '.btnUser', loadMain);
 $('.sidebar').on('click', '#btnMessage', loadMessageLanding);
-$('#sidebarSwiper').swipe({
+$('.sidebar').on('click', '#btnBackend', loadBackend);
+$('.sidebar').on('click', '.btnBack', loadMain)
+$('.sidebarSwiper').swipe({
   swipeStatus:function(event,phase,direction,distance,duration,fingers) {
+    //console.log(direction);
     if(phase=='move' && direction=="right") {
-      $('.sidebar').addClass("open-sidebar");
-      setTimeout(function(){
-        console.log('close the sidebar');
-        $('.sidebar').removeClass('open-sidebar');
-      }, sideBarTimeout);
+      openSidebar();
       return false;
     }
     if(phase=='move' && direction=='left') {
-      $('.sidebar').removeClass('open-sidebar');
+      closeSidebar();
       return false;
     }
   }
 });
 
 //###FUNCTIONS#################################
-
+function loadMain(event) {
+  //closeSidebar();
+  showSidebar('sidebarMain');
+  console.log('goTo: main page');
+  $('#header').text('header');
+}
 function loadMessageLanding(event) {
-  console.log('Messages');
-  $('#content').text('Messages');
+  closeSidebar();
+  console.log('goTo: message landing');
+  $('#header').text('Message Landingpage');
+  //TODO: change the sidebar to the create Message sidebar
+}
+function loadBackend(event) {
+  $('#header').text('Backend');
+  showSidebar('sidebarBackend');
+  updateTimer();
+  //$('#sidebarMain').css('visibility', 'hidden');
+  //$('#sidebarBackend').css('visibility', 'visible');
+}
+
+function openSidebar(){
+  updateTimer();
+  $('.sidebar').removeClass('close-sidebar');
+  $('.sidebar').addClass("open-sidebar");
+};
+function closeSidebar() {
+  $('.sidebar').removeClass('open-sidebar');
+  $('.sidebar').addClass("close-sidebar");
+  clearTimeout(sidebarTimeoutHandler);
+}
+
+function showSidebar(sidebar) {
+  switch(sidebar){
+    case 'sidebarMain':
+      //all contents
+      $('#sidebarMain').css('visibility', 'visible');
+      $('#sidebarBackend').css('visibility', 'hidden');
+      break;
+    case 'sidebarBackend':
+      $('#sidebarMain').css('visibility', 'hidden');
+      $('#sidebarBackend').css('visibility', 'visible');
+    default:
+      //nothing or main???
+      break;
+  };
+}
+function updateTimer() {
+  clearTimeout(sidebarTimeoutHandler);
+  sidebarTimeoutHandler = setTimeout(closeSidebar, sideBarTimeout);
 }
