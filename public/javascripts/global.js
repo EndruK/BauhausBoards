8fc31e3c25eb0937644d5b186550840bdb8eac9a
André Karge <andre.karge@gmail.com>
2015-07-02 16:46:51 +0200

    added automatic sidebar closing

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,14 +1,20 @@
 var pos = 'index';
+var sideBarTimeout = 20000; //sidebar timeout in ms
+
 //###DOM#######################################
 $('.sidebar').on('click', '#btnMessage', loadMessageLanding);
 $('#sidebarSwiper').swipe({
   swipeStatus:function(event,phase,direction,distance,duration,fingers) {
     if(phase=='move' && direction=="right") {
       $('.sidebar').addClass("open-sidebar");
+      setTimeout(function(){
+        console.log('close the sidebar');
+        $('.sidebar').removeClass('open-sidebar');
+      }, sideBarTimeout);
       return false;
     }
-    if(phase=="move" && direction=="left") {
-      $('.sidebar').removeClass("open-sidebar");
+    if(phase=='move' && direction=='left') {
+      $('.sidebar').removeClass('open-sidebar');
       return false;
     }
   }
