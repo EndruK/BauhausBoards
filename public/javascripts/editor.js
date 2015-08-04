4b1613e1924a3f66db35dd5865fe3a9ef650095e
André Karge <andre.karge@gmail.com>
2015-08-04 14:31:00 +0200

    first drawing works

--- public/javascripts/editor.js
+++ public/javascripts/editor.js
@@ -1,29 +1,18 @@
-$('.sidebar').on('click', '.test', test);
-$('.sidebar').on('click', '.test2', test2);
-$('.sidebar').on('click', '.refresh', refreshCanvas);
-var canvas = document.getElementById('myCanvas');
-paper.setup(canvas);
+//$('.sidebar').on('click', '.test', test);
+//$('.sidebar').on('click', '.test2', test2);
+//$('.sidebar').on('click', '.refresh', refreshCanvas);
 
-function test(event) {
-  console.log("drawing line");
-  var path = new paper.Path();
-  path.strokeColor = 'black';
-  var start = new paper.Point(100,100);
-  path.moveTo(start);
-  path.lineTo(start.add([ 200, -50 ]));
-  paper.view.draw();
-}
-function test2(event) {
-  console.log("drawing circle");
-  var path2 = new Path.Circle({
-    center: view.center,
-    radius: 30,
-    strokeColor: 'black'
-  });
-  paper.view.draw();
-}
-function refreshCanvas(event) {
-  console.log("refreshing the canvas");
-  paper.setup(canvas);
-  paper.view.draw();
-}
+paper.install(window);
+$(document).ready(function() {
+  paper.setup('myCanvas');
+  var tool = new Tool();
+  var path;
+  tool.onMouseDown = function(event) {
+    path = new Path();
+    path.strokeColor = 'black';
+    path.add(event.point);
+  }
+  tool.onMouseDrag = function(event) {
+    path.add(event.point);
+  }
+});
