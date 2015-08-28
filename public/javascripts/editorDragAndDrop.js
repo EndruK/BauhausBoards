d92373025dbed3420b8ffda728016a99c612844d
André Karge <andre.karge@uni-weimar.de>
2015-08-28 15:57:04 +0200

    added a loading animation

--- public/javascripts/editorDragAndDrop.js
+++ public/javascripts/editorDragAndDrop.js
@@ -1,6 +1,6 @@
 //set up the global paper scope
 paper.install(window);
-
+var uploading = false;
 function drag(event) {
   console.log("drag");
   event.stopPropagation();
@@ -12,8 +12,6 @@ function drop(event) {
   event.stopPropagation();
   event.preventDefault();
   console.log(event);
-  event.stopPropagation();
-  event.preventDefault();
   //get image url from browser drop
   if(event.originalEvent.dataTransfer.types.length > 0 && event.originalEvent.dataTransfer.getData('URL') != "") {
     var imgURL = event.originalEvent.dataTransfer.getData('URL');
@@ -33,6 +31,8 @@ function drop(event) {
       r.onload = function(event) {
         var result = event.target.result;
         var image  = result.substr(result.indexOf(",") + 1);
+        uploading = true;
+        showLoadingGif();
         //do ajax to imgur
         $.ajax({
           url: "https://api.imgur.com/3/image",
@@ -51,6 +51,10 @@ function drop(event) {
           error: function(input) {
             console.log(input);
             alert("Error while uploading image!");
+          },
+          complete: function() {
+            removeLoadingGif();
+            uploading = false;
           }
         });
       }
@@ -69,5 +73,25 @@ function drawImage(imgURL) {
       raster2.scale(0.3);
     }
     addGifTicker();
+    activateSelectorTool();
   }
+}
+function showLoadingGif() {
+  console.log("show gif");
+  $("#wrapper").append("<div id='loadingAnimation'>");
+  var loadingAnimation = $("#loadingAnimation");
+  loadingAnimation.append("<span id='uploadImage' class='glyphicon glyphicon-refresh glyphicon-refresh-animate'>");
+  var spanEl = $("#uploadImage");
+  spanEl.css("font-size","1.5em");
+  var left = $(window).width()/2 - loadingAnimation.width()/2;
+  var top = $(window).height()/2 - loadingAnimation.height()/2;
+  loadingAnimation.css({
+    display: "inline-block",
+    left: left+"px",
+    top:  top+"px"
+  });
+}
+function removeLoadingGif() {
+  console.log("remove gif");
+  $("#loadingAnimation").remove();
 }
