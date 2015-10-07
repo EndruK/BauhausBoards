7a62bb2c19cdd1d8e9535002cb75b221127ebdc4
André Karge <andre.karge@gmail.com>
2015-10-07 15:16:07 +0200

    fixed the double image upload bug

--- public/javascripts/editor/editorDragAndDrop.js
+++ public/javascripts/editor/editorDragAndDrop.js
@@ -20,46 +20,44 @@ function drop(event) {
   }
 
   //upload image first
-  else if(event.originalEvent.dataTransfer.files.length > 0) {
+  else if(event.originalEvent.dataTransfer.files.length == 1) {
     //user has more than one element uploaded?
     var files = event.originalEvent.dataTransfer.files;
     console.log(files);
-    for(var i=0; i< files.length; i++) {
-      var key = files[i];
-      var r = new FileReader();
-      var url;
-      r.onload = function(event) {
-        var result = event.target.result;
-        var image  = result.substr(result.indexOf(",") + 1);
-        uploading = true;
-        showLoadingGif();
-        //do ajax to imgur
-        $.ajax({
-          url: "https://api.imgur.com/3/image",
-          type: "post",
-          headers: {
-            Authorization: "Client-ID 395d5a7dec70b4a"
-          },
-          data: {
-            image: image
-          },
-          dataType: "json",
-          success: function(result,status){
-            url = result.data.link;
-            drawImage(url);
-          },
-          error: function(input) {
-            console.log(input);
-            alert("Error while uploading image!");
-          },
-          complete: function() {
-            removeLoadingGif();
-            uploading = false;
-          }
-        });
-      }
-      r.readAsDataURL(key);
+    var key = files[0];
+    var r = new FileReader();
+    var url;
+    r.onload = function(event) {
+      var result = event.target.result;
+      var image  = result.substr(result.indexOf(",") + 1);
+      uploading = true;
+      showLoadingGif();
+      //do ajax to imgur
+      $.ajax({
+        url: "https://api.imgur.com/3/image",
+        type: "post",
+        headers: {
+          Authorization: "Client-ID 395d5a7dec70b4a"
+        },
+        data: {
+          image: image
+        },
+        dataType: "json",
+        success: function(result,status){
+          url = result.data.link;
+          drawImage(url);
+        },
+        error: function(input) {
+          console.log(input);
+          alert("Error while uploading image!");
+        },
+        complete: function() {
+          removeLoadingGif();
+          uploading = false;
+        }
+      });
     }
+    r.readAsDataURL(key);
   }
 }
 function checkURL(url) {
