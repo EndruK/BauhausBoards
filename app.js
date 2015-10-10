6a29b596f8326b9333cbfbc573198353283f10f2
André Karge <andre.karge@uni-weimar.de>
2015-10-11 00:10:00 +0200

    favicon

--- app.js
+++ app.js
@@ -65,7 +65,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
+//app.use(favicon(path.join(__dirname,'public','images','favicon.ico'));
 app.use(logger('dev'));
 //define stylus to compile styl to css
 app.use(stylus.middleware(
