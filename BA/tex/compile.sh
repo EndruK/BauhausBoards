f5aa54794c7dce4ef5ae8677aff564ace31aa93c
André Karge <woxi3024@webis40.medien.uni-weimar.de>
2015-11-08 18:59:22 +0100

    hermes almost done

--- BA/tex/compile.sh
+++ BA/tex/compile.sh
@@ -8,15 +8,15 @@ function compile() {
   pdflatex $file
   pdflatex $file
 
-  for f in * ; do
-    filename="${f%.*}"
-    extension="${f##*.}"
-    if [[ ( ( ! -d $f ) && ( $extension != "tex") && ( $extension != "zip") && ( $extension != "pdf" ) && ( $extension != "bib" ) && ( $extension != "cls" ) && ( $extension != "png" ) && ( $extension != "jpg" ) && ( $extension != "sh" ) && ( $extension != "bst" ) ) ]] ; then
-      #echo "$filename   $extension"
-      rm $f
-    fi
-  done
-
+  rm -f *.toc *.aux *.log *.out *.brf *.blg *.bbl
+  #for f in * ; do
+  #  filename="${f%.*}"
+  #  extension="${f##*.}"
+  #  if [[ ( ( ! -d $f ) && ( $extension != "tex") && ( $extension != "zip") && ( $extension != "pdf" ) && ( $extension != "bib" ) && ( $extension != "cls" ) && ( $extension != "png" ) && ( $extension != "jpg" ) && ( $extension != "sh" ) && ( $extension != "bst" ) ) ]] ; then
+  #    #echo "$filename   $extension"
+  #    rm $f
+  #  fi
+  #done
 }
 
 compile thesis.tex
